a546fd8efb8c8826f598b905dff23a5670f410c3
Igor Mourao Gomes <igormgomes94@gmail.com>
2017-11-23 00:02:35 -0200

    imports nao usados

--- src/main/java/br/com/reclamacao/service/api/controller/ReclamacaoController.java
+++ src/main/java/br/com/reclamacao/service/api/controller/ReclamacaoController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import javax.xml.ws.Response;
 import java.net.URI;
 import java.util.Set;
 
